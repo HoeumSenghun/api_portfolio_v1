20f2909439681954eea9a9ad7c4c0190942e0db1
HoeumSenghun <hoeumsenghun369@gmail.com>
2026-02-02 16:07:21 +0700

    init project

---
+++ src/test/java/com/portfolio_v1/api_v1/ApiPortfolioV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio_v1.api_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPortfolioV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
